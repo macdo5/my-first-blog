2b3799d5183ec97a78d5373766ca621bdf6478ae
Oscar Macdonald <macdo5@student.op.ac.nz>
2017-05-09 15:55:46 +1200

    created URLs, views and templates for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post) # need to register the model to make it visible in the admin page
+# customizes the interface
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','created_date','published_date']
+
+admin.site.register(Post, PostAdmin) # need to register the model to make it visible in the admin page
